7e069a64d3152b92a3fa6eabcdbf2ca9592bdb0b
alixiazul <alicia.rodriguez.t@gmail.com>
2024-06-10 16:16:54 +0100

    Refactor so the conditionals also check for values different from 0s.

--- src/tic_tac_toe_checker.py
+++ src/tic_tac_toe_checker.py
@@ -21,27 +21,22 @@ def is_solved(board):
 
     # Check rows
     for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2]:
-            if board[i][0] != 0:
-                return board[i][0]
+        if board[i][0] == board[i][1] == board[i][2] != 0:
+            return board[i][0]
     # Check columns
-        if board[0][i] == board[1][i] == board[2][i]:
-            if board[0][i] != 0:
-                return board[0][i]
+        if board[0][i] == board[1][i] == board[2][i] != 0:
+            return board[0][i]
 
     # Check diagonals
-    if board[0][0] == board[1][1] == board[2][2]:
-        if board[0][0] != 0:
-            return board[0][0]
+    if board[0][0] == board[1][1] == board[2][2] != 0:
+        return board[0][0]
 
-    if board[0][2] == board[1][1] == board[2][0]:
-        if board[0][0] != 0:
-            return board[0][2]
+    if board[0][2] == board[1][1] == board[2][0] != 0:
+        return board[0][2]
 
     # Check continuing game, not yet finished:
     for row in board:
         for element in row:
-            print(element)
             if element == 0:
                 return -1
 
