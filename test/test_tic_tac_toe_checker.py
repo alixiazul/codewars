a1426af6981d4b8f16cf18e0ba5be69d5303002a
alixiazul <alicia.rodriguez.t@gmail.com>
2024-06-10 14:33:51 +0100

    Implement the tic tac toe game

---
+++ test/test_tic_tac_toe_checker.py
@@ -0,0 +1,34 @@
+from src.tic_tac_toe_checker import is_solved
+
+
+def test_game_not_yet_finished():
+    # not yet finished
+    board = [[0, 0, 1],
+             [0, 1, 2],
+             [2, 1, 0]]
+
+    assert is_solved(board) == -1
+
+
+def test_game_winning_row():
+    # winning row
+    board = [[1, 1, 1],
+             [0, 2, 2],
+             [0, 0, 0]]
+    assert is_solved(board) == 1
+
+
+def test_game_winning_column():
+    # winning column
+    board = [[2, 1, 2],
+             [2, 1, 1],
+             [1, 1, 2]]
+    assert is_solved(board) == 1
+
+
+def test_game_draw():
+    # draw
+    board = [[2, 1, 2],
+             [2, 1, 1],
+             [1, 2, 1]]
+    assert is_solved(board) == 0
