c2d5732a3a030e95f70a382e1eb9d2dd7177f39b
alixiazul <alicia.rodriguez.t@gmail.com>
2024-06-10 16:11:10 +0100

    Add new test case where there is a not-ended board with a row full of zeros

--- test/test_tic_tac_toe_checker.py
+++ test/test_tic_tac_toe_checker.py
@@ -1,11 +1,20 @@
 from src.tic_tac_toe_checker import is_solved
 
 
-def test_game_not_yet_finished():
+def test_game_not_yet_finished_1():
     # not yet finished
     board = [[0, 0, 1],
              [0, 1, 2],
              [2, 1, 0]]
+
+    assert is_solved(board) == -1
+
+
+def test_game_not_yet_finished_2():
+    # not yet finished
+    board = [[0, 0, 2],
+             [0, 0, 0],
+             [1, 0, 1]]
 
     assert is_solved(board) == -1
 
